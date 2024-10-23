851958edda91b181233ad45b13c9b04ddf82ad08
akumarucode <akmalaminincoc@gmail.com>
2024-10-23 23:04:09 +0800

    web API for 2dFoxGame
    Signed-off-by: akumarucode <akmalaminincoc@gmail.com>

---
+++ 2dFoxGameAPI/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace _2dFoxGameAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromForm] string username, [FromForm] string password)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT COUNT(1) FROM users WHERE username=@Username AND password=@Password", conn);
+                cmd.Parameters.AddWithValue("@Username", username);
+                cmd.Parameters.AddWithValue("@Password", password);
+
+                int userCount = (int)cmd.ExecuteScalar();
+                if (userCount == 1)
+                {
+                    return Ok(new { message = "Login successful" });
+                }
+                else
+                {
+                    return Unauthorized(new { message = "Invalid credentials" });
+                }
+            }
+        }
+    }
+}
